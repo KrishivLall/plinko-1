3d417aa46e3041af600311bd7edb6818dde935ee
KrishivLall <68279115+KrishivLall@users.noreply.github.com>
2020-12-01 18:37:17 +0530

    Add files via upload

--- particle.js
+++ particle.js
@@ -9,17 +9,22 @@ class Particle {
         }
         
         this.radius = radius;
-        this.body = Bodies.circle(x, y, radius, options);
+        this.body = Bodies.circle(x, y, this.radius, options);
+        this.color = color(random(0, 225), random(0, 225), random(0, 225))
         World.add(world , this.body);
     }
     display(){
+      
       var pos = this.body.position
       var angle = this.body.angle;
+
       push();
       rotate(angle);
       translate(pos.x, pos.y);
+      noStroke();
+      fill(this.color);
       ellipseMode(RADIUS);
-      fill(225);
+      //fill(this.color);
       ellipse(0, 0, this.radius, this.radius)
       pop();
     }
--- sketch.js
+++ sketch.js
@@ -19,37 +19,55 @@ function setup() {
   divisions.push(new Division(k, height = divisionHeight*2, 10, divisionHeight));
   }
 
-  for( var j = 40; width <= 400; j = j+50){
+  for( var j = 40; j <= 400; j = j+50){
   plinkos.push(new Plinko(j,75));
   }
- /*
-  for( var j = 35; width >= 400; j = j+50){
+ 
+  for( var j = 35; j <= 400; j = j+50){
     plinkos.push(new Plinko(j,175, 20));
   }
 
-  for( var j = 30; width >= 400; j = j+50){
+  for( var j = 30; j <= 400; j = j+50){
     plinkos.push(new Plinko(j,275, 20));
   }
 
-  for( var j = 25; width >= 400; j = j+50){
+  for( var j = 25; j <= 400; j = j+50){
     plinkos.push(new Plinko(j,375, 20));
   }
- */
- // if(frameCount%60===0){
-   // particles.push(new Particle(random(width/2-10, width/2+10), 10, 10));
- // }
+ 
+  ground = new Ground(240, 740, 500, 50);
+
+  Engine.run(engine);
 }
 
 
 
 function draw() {
 
-  background("black");   
-  Engine.update(engine);
+  
 
-    for(var k = 0; k <= divisions.length; k++ ){
+  background("black");   
+  
+
+    for(var k = 0; k<divisions.length; k++ ){
     divisions[k].display();
    }
+
+   for(var k = 0; k <plinkos.length; k++){
+     plinkos[k].display();
+   }
+
+
+
+   if(frameCount%60===0){
+    particles.push(new Particle(random(width/2-10, width/2+10), 10, 10));
+    }
+
+    for(var k = 0;k <particles.length; k++){
+      particles[k].display();
+    }
+
+   ground.display();
 
     
 }
